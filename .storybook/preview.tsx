3c1620b094d9ba2cda505274fe136f4774f1a57f
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-29 14:31:06 +0200

    add storybook preview config
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ .storybook/preview.tsx
@@ -0,0 +1,24 @@
+import { addDecorator } from "@storybook/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import "styles/fonts.css";
+import { GlobalStyle } from "components";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+const queryClient = new QueryClient();
+
+addDecorator((Story) => (
+  <QueryClientProvider client={queryClient}>
+    <GlobalStyle />
+    <Story />
+  </QueryClientProvider>
+));
