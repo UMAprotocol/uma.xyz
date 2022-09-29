0a2962dd9ae23adfcf24251ea01cdd3cf5fe2b4d
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-29 11:00:55 +0200

    add react query
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools/build/lib/devtools";
 import { GlobalStyle } from "components/GlobalStyle/GlobalStyle";
 import type { AppProps } from "next/app";
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
