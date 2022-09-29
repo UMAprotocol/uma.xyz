2fd5d41b806b996c872c348b686a442c27737cde
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-29 14:18:43 +0200

    import fonts in app
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools/build/lib/devtools";
 import { GlobalStyle } from "components";
 import type { AppProps } from "next/app";
+import "styles/fonts.css";
 
 const queryClient = new QueryClient();
 
