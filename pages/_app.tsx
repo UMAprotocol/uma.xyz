17d0b74323ef778a3afe1169094bc24a62b5558c
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-29 10:53:11 +0200

    add global style with reset
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import type { AppProps } from 'next/app'
+import { GlobalStyle } from "components/GlobalStyle/GlobalStyle";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
