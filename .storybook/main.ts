5b4f8e8883a1cdf03f96a0eff57bb6d178a1915f
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-30 11:17:51 +0200

    add svg webpack rule for storybook
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,6 +14,26 @@ const config: StorybookConfig = {
   core: {
     builder: "@storybook/builder-webpack5",
   },
+  webpackFinal: async (config) => {
+    // this modifies the existing image rule to exclude .svg files
+    // since we want to handle those files with @svgr/webpack
+    const imageRule = config?.module?.rules?.find((rule) => {
+      if (!rule || rule === "...") return;
+      if (rule.test instanceof RegExp) return rule.test.test(".svg");
+    });
+
+    if (imageRule && imageRule !== "...") {
+      imageRule.exclude = /\.svg$/;
+    }
+
+    // configure .svg files to be loaded with @svgr/webpack
+    config?.module?.rules?.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 module.exports = config;
