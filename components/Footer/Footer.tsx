f59f7303a30a01e334c64f041635180306f8bd2d
Tulun <jaykiraly@gmail.com>
2022-10-12 15:41:53 -0700

    Social icons
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,31 +1,92 @@
 import styled from "styled-components";
 import Logo from "public/assets/uma-logo.svg";
-
+import Twitter from "public/assets/twitter.svg";
+import Discord from "public/assets/discord.svg";
 const Footer = () => {
   return (
     <Wrapper>
-      <TopRow>
+      <Row>
         <Logo />
-      </TopRow>
+      </Row>
+      <BottomRow>
+        <FooterLinks>
+          <SocialLinks>
+            {socialLinks.map(({ href, Icon }, i) => (
+              <SocialLink key={i} href={href} rel="noreferrer" target="_blank">
+                <Icon />
+              </SocialLink>
+            ))}
+          </SocialLinks>
+        </FooterLinks>
+      </BottomRow>
     </Wrapper>
   );
 };
 
 export default Footer;
 
-const Wrapper = styled.footer`
+const socialLinks = [
+  {
+    href: "https://twitter.com/UMAprotocol",
+    Icon: Twitter,
+  },
+  {
+    href: "http://discord.umaproject.org",
+    Icon: Discord,
+  },
+  {
+    href: "https://twitter.com/UMAprotocol",
+    Icon: Twitter,
+  },
+  {
+    href: "http://discord.umaproject.org",
+    Icon: Discord,
+  },
+  {
+    href: "https://twitter.com/UMAprotocol",
+    Icon: Twitter,
+  },
+  {
+    href: "http://discord.umaproject.org",
+    Icon: Discord,
+  },
+];
+
+const Wrapper = styled.div`
   background: var(--black);
   width: 100%;
-  margin-top: 96px;
   max-width: 1440px;
-  margin: 0 auto;
+  margin: 96px auto 0;
 `;
 
-const TopRow = styled.div`
+const Row = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  padding: 8px 16px 8px 8px;
+  padding: 8px 16px 8px 0px;
   gap: 16px;
+`;
+
+const FooterLinks = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const SocialLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`;
+
+const SocialLink = styled.a`
+  &:hover {
+    path {
+      fill: var(--white);
+    }
+  }
+`;
+
+const BottomRow = styled(Row)`
+  margin-top: 110px;
 `;
