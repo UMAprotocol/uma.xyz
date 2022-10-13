0c01ebe3e2b536a70459534e35418dc4fd43eff1
Tulun <jaykiraly@gmail.com>
2022-10-12 17:09:27 -0700

    footer story
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ stories/Footer.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Footer } from "components";
+
+export default {
+  title: "Footer",
+  component: Footer,
+} as ComponentMeta<typeof Footer>;
+
+const Template: ComponentStory<typeof Footer> = () => (
+  <div style={{ padding: "1rem 2rem", background: "var(--black)" }}>
+    <Footer />
+  </div>
+);
+
+export const Main = Template.bind({});
+Main.args = {};
