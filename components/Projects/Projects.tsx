6c47290a59d15d20d968c9b051c308c88d82ffd8
Tulun <jaykiraly@gmail.com>
2022-10-14 10:39:59 -0700

    Stub
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ components/Projects/Projects.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Projects = () => {
+  return (
+    <Section>
+      <Wrapper></Wrapper>
+    </Section>
+  );
+};
+
+export default Projects;
+
+const Section = styled.section`
+  width: 100%;
+  background: var(--white);
+`;
+const Wrapper = styled.div`
+  background: inherit;
+  width: 100%;
+  max-width: 1144px;
+  margin: 128px auto 117px;
+`;
