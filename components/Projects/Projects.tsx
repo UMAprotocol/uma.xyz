2b8021bba378f08f4494b2773b1399ac48eb1a18
Tulun <jaykiraly@gmail.com>
2022-10-14 10:52:18 -0700

    start of flex grid
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -3,7 +3,16 @@ import styled from "styled-components";
 const Projects = () => {
   return (
     <Section>
-      <Wrapper></Wrapper>
+      <Wrapper>
+        <ProjectsRow>
+          <ProjectsColumn>
+            <BigProjects>
+              <BigProject></BigProject>
+            </BigProjects>
+          </ProjectsColumn>
+          <ProjectsText></ProjectsText>
+        </ProjectsRow>
+      </Wrapper>
     </Section>
   );
 };
@@ -20,3 +29,24 @@ const Wrapper = styled.div`
   max-width: 1144px;
   margin: 128px auto 117px;
 `;
+
+const ProjectsRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const ProjectsColumn = styled.div``;
+const BigProjects = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
+const BigProject = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-basis: 50%;
+`;
+const ProjectsText = styled.div``;
