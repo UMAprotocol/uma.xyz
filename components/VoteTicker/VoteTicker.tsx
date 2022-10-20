6121c7e52559b1fcbd5aeb4fa8dd2139f1e2a964
Tulun <jaykiraly@gmail.com>
2022-10-20 13:54:53 -0700

    clock svg
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- components/VoteTicker/VoteTicker.tsx
+++ components/VoteTicker/VoteTicker.tsx
@@ -1,5 +1,29 @@
+import styled from "styled-components";
 const VoteTicker = () => {
-  return <div />;
+  return <Wrapper></Wrapper>;
 };
 
 export default VoteTicker;
+
+const Wrapper = styled.div`
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 16px 8px 8px;
+  gap: 16px;
+  isolation: isolate;
+
+  position: absolute;
+  width: 1144px;
+  height: 48px;
+  left: 148px;
+  top: 16px;
+
+  /* Gray/Gray-200 */
+
+  background: #322f33;
+  border-radius: 8px;
+`;
