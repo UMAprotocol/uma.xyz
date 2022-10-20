6657875a0dcb959e1bdd211b6ead848199bfbf4d
Tulun <jaykiraly@gmail.com>
2022-10-20 13:49:59 -0700

    story
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ stories/SupportSection.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { SupportSection } from "components";
+import "../styles/sandpack-override.scss";
+
+export default {
+  title: "SupportSection",
+  component: SupportSection,
+} as ComponentMeta<typeof SupportSection>;
+
+const Template: ComponentStory<typeof SupportSection> = () => (
+  <div style={{ padding: "2rem", background: "var(--black)" }}>
+    <SupportSection />
+  </div>
+);
+
+export const Main = Template.bind({});
+Main.args = {};
