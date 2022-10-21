ac932922006ff5b6f74980397f3e4541bee73877
Tulun <jaykiraly@gmail.com>
2022-10-21 11:32:24 -0700

    rename props
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- components/VoteTicker/VoteTicker.tsx
+++ components/VoteTicker/VoteTicker.tsx
@@ -1,37 +1,49 @@
-import { useState, useEffect } from "react";
-import styled from "styled-components";
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
 import Clock from "public/assets/clock.svg";
 import UpRightArrow from "public/assets/up-right-arrow.svg";
 import { formatDateTimeFromUTC } from "./utils";
 import useInterval from "hooks/helpers/useInterval";
-type theme = "light" | "dark";
+type TickerThemes = "light" | "dark";
 
 interface Props {
-  theme: theme;
+  theme: TickerThemes;
 }
+
+// styled theme
+const st = {
+  light: {
+    background: "var(--white-50)",
+  },
+  dark: {
+    wrapperBg: "var(--black)",
+  },
+};
 
 const VoteTicker: React.FC<Props> = ({ theme }) => {
   const { timeRemaining } = useVoteTicker();
   return (
-    <Section theme={theme}>
-      <Wrapper theme={theme}>
-        <VoteBlock theme={theme}>
-          <ClockBG theme={theme}>
-            <Clock />
-          </ClockBG>
-          <VoteText theme={theme}>
-            Time to commit vote:
-            <span>{timeRemaining}</span>
-          </VoteText>
-        </VoteBlock>
-        <MoreDetailsBlock theme={theme}>
-          <span>More details</span>
-          <a href="https://vote.umaproject.org/" target="_blank" rel="noreferrer">
-            <UpRightArrow />
-          </a>
-        </MoreDetailsBlock>
-      </Wrapper>
-    </Section>
+    <ThemeProvider theme={st}>
+      <Section themeType={theme}>
+        <Wrapper themeType={theme}>
+          <VoteBlock>
+            <ClockBG themeType={theme}>
+              <Clock />
+            </ClockBG>
+            <VoteText themeType={theme}>
+              Time to commit vote:
+              <span>{timeRemaining}</span>
+            </VoteText>
+          </VoteBlock>
+          <MoreDetailsBlock themeType={theme}>
+            <span>More details</span>
+            <a href="https://vote.umaproject.org/" target="_blank" rel="noreferrer">
+              <UpRightArrow />
+            </a>
+          </MoreDetailsBlock>
+        </Wrapper>
+      </Section>
+    </ThemeProvider>
   );
 };
 
@@ -50,13 +62,13 @@ function useVoteTicker() {
 }
 
 interface IStyledProps {
-  theme: theme;
+  themeType: TickerThemes;
 }
 
 const Section = styled.div<IStyledProps>`
   width: 100%;
-  background: ${({ theme }) => (theme === "dark" ? "inherit" : "var(--grey-100)")};
-  padding-top: ${({ theme }) => (theme === "dark" ? "16px" : "48px")}; ;
+  background: ${({ themeType }) => (themeType === "dark" ? "inherit" : "var(--grey-100)")};
+  padding-top: ${({ themeType }) => (themeType === "dark" ? "16px" : "48px")}; ;
 `;
 
 const Wrapper = styled.div<IStyledProps>`
@@ -70,7 +82,7 @@ const Wrapper = styled.div<IStyledProps>`
   max-width: var(--max-section-width);
   margin: 0 auto;
   height: 48px;
-  background: ${({ theme }) => (theme === "dark" ? "var(--black-100)" : "var(--white-50)")};
+  background: ${({ themeType }) => (themeType === "dark" ? "var(--black-100)" : "var(--white-50)")};
   border-radius: 8px;
 `;
 
@@ -91,35 +103,35 @@ const ClockBG = styled.div<IStyledProps>`
   width: 32px;
   height: 32px;
   background: var(--red-510-opacity-15);
-  border-radius: ${({ theme }) => (theme === "dark" ? "16px" : "4px")};
+  border-radius: ${({ themeType }) => (themeType === "dark" ? "16px" : "4px")};
   g {
     fill: var(--red-550);
-    fill: ${({ theme }) => (theme === "dark" ? "var(--red-550)" : "var(--red-510-opacity-15)")};
+    fill: ${({ themeType }) => (themeType === "dark" ? "var(--red-550)" : "var(--red-510-opacity-15)")};
   }
 `;
 
 const VoteText = styled.div<IStyledProps>`
   font: var(--body-sm);
-  color: ${({ theme }) => (theme === "dark" ? "var(--grey-910)" : "var(--black-150)")};
+  color: ${({ themeType }) => (themeType === "dark" ? "var(--grey-910)" : "var(--black-150)")};
   span {
-    color: ${({ theme }) => (theme === "dark" ? "var(--white)" : "var(--red-550)")};
+    color: ${({ themeType }) => (themeType === "dark" ? "var(--white)" : "var(--red-550)")};
     color: var(--red-500);
     margin-left: 4px;
   }
   padding-right: 16px;
-  border-right: ${({ theme }) =>
-    theme === "dark" ? "1px solid var(--grey-opacity-20)" : "1px solid var(--black-150-opacity-20)"};
+  border-right: ${({ themeType }) =>
+    themeType === "dark" ? "1px solid var(--grey-opacity-20)" : "1px solid var(--black-150-opacity-20)"};
 `;
 
 const MoreDetailsBlock = styled.div<IStyledProps>`
   font: var(--body-sm);
-  color: ${({ theme }) => (theme === "dark" ? "var(--grey-910)" : "var(--black-150)")};
+  color: ${({ themeType }) => (themeType === "dark" ? "var(--grey-910)" : "var(--black-150)")};
   display: flex;
   flex-direction: row;
   align-items: center;
   padding: 0px;
   gap: 4px;
   path {
-    stroke: ${({ theme }) => (theme === "dark" ? "var(--grey-910)" : "var(--black-150)")};
+    stroke: ${({ themeType }) => (themeType === "dark" ? "var(--grey-910)" : "var(--black-150)")};
   }
 `;
