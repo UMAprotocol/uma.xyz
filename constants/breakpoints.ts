a63186c5a6356b09031e06d415c460c6b9e30193
Tulun <jaykiraly@gmail.com>
2022-10-27 14:19:12 -0700

    breakpoints
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- constants/breakpoints.ts
+++ constants/breakpoints.ts
@@ -1,1 +1,18 @@
 export const maxSectionWidth = "1140px";
+// Configures the V2 breakpoints
+export const BREAKPOINTS = {
+  xs: 400,
+  sm: 576,
+  md: 714,
+  tb: 1024,
+};
+const breakpoint = (width: number) => ({
+  andDown: `(max-width: ${width}px)`,
+  andUp: `(min-width: ${width}px)`,
+});
+export const QUERIESV2 = {
+  xs: breakpoint(BREAKPOINTS.xs),
+  sm: breakpoint(BREAKPOINTS.sm),
+  md: breakpoint(BREAKPOINTS.md),
+  tb: breakpoint(BREAKPOINTS.tb),
+};
