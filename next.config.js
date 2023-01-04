fafe1a8057e3133e80f640588e68c410a29eed58
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2023-01-04 09:24:45 -0500

    fix: optimize images (#46)
    * fix: optimize images
    
    * improve: modify config to use defaults

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: { styledComponents: true },
-  images: {
-    unoptimized: true,
-  },
   eslint: {
     dirs: ["components", "constants", "hooks", "pages", "queries", "stories", "types"],
   },
