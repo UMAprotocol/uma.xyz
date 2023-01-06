549832eb6fcdc6aa05d3f7ed0ed8ecb998540ea0
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-06 09:48:56 +0200

    add entry animation
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/SectionHeader/SectionHeader.tsx
+++ components/SectionHeader/SectionHeader.tsx
@@ -1,9 +1,10 @@
-import { mobileAndUnder, laptopAndUnder, tabletAndUnder } from "constant";
-import styled from "styled-components";
+import { laptopAndUnder, mobileAndUnder, tabletAndUnder } from "constant";
+import { motion } from "framer-motion";
+import styled, { CSSProperties } from "styled-components";
 
 const HeaderWrapper = styled.div``;
 
-const Title = styled.h1`
+const Title = styled(motion.h1)`
   font: var(--header-sm);
   padding-bottom: 16px;
   border-bottom: 1px solid var(--grey-600);
@@ -12,13 +13,12 @@ const Title = styled.h1`
   }
 `;
 
-const Header = styled.h2<{ constrainWidth?: boolean }>`
+const Header = styled(motion.h2)`
   --width-desktop-tablet: 1020px;
   --width-laptop: 720px;
   --width-mobile: 100%;
   --width: var(--width-desktop-tablet);
-  max-width: var(--width);
-  max-width: ${({ constrainWidth }) => (constrainWidth ? "max(70%, 720px)" : "var(--width)")};
+  max-width: var(--max-width);
   margin-top: 48px;
   margin-bottom: 128px;
   font: var(--header-lg);
@@ -53,7 +53,12 @@ type SectionHeaderProps = {
 export function SectionHeader({ title, header, constrainWidth }: SectionHeaderProps) {
   return (
     <HeaderWrapper>
-      <Title>
+      <Title
+        initial={{ opacity: 0.1, rotate: "-2deg" }}
+        whileInView={{ opacity: 1, rotate: "0deg" }}
+        viewport={{ once: true, amount: "all" }}
+        transition={{ duration: 0.6 }}
+      >
         {typeof title === "string" ? (
           title
         ) : (
@@ -62,7 +67,19 @@ export function SectionHeader({ title, header, constrainWidth }: SectionHeaderPr
           </>
         )}
       </Title>
-      <Header constrainWidth={constrainWidth}>{header}</Header>
+      <Header
+        initial={{ opacity: 0.1, rotate: "1deg" }}
+        whileInView={{ opacity: 1, rotate: "0deg" }}
+        viewport={{ once: true, amount: "all" }}
+        transition={{ duration: 0.6 }}
+        style={
+          {
+            "--max-width": constrainWidth ? "max(70%, 720px)" : "var(--width)",
+          } as CSSProperties
+        }
+      >
+        {header}
+      </Header>
     </HeaderWrapper>
   );
 }
