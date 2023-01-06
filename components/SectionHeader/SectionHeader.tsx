d0c5eec3b2bbe06564473cce536a9e285ed9ace4
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-06 14:01:59 +0200

    add circle filter animation to section headers
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/SectionHeader/SectionHeader.tsx
+++ components/SectionHeader/SectionHeader.tsx
@@ -1,8 +1,11 @@
 import { laptopAndUnder, mobileAndUnder, tabletAndUnder } from "constant";
 import { motion } from "framer-motion";
+import { useState } from "react";
 import styled, { CSSProperties } from "styled-components";
 
-const HeaderWrapper = styled.div``;
+const HeaderWrapper = styled.div`
+  position: relative;
+`;
 
 const Title = styled(motion.h1)`
   font: var(--header-sm);
@@ -48,9 +51,10 @@ type SectionHeaderProps = {
       };
   header: string | JSX.Element;
   constrainWidth?: boolean;
+  hasCircleFilter?: boolean;
 };
 
-export function SectionHeader({ title, header, constrainWidth }: SectionHeaderProps) {
+export function SectionHeader({ title, header, constrainWidth, hasCircleFilter = true }: SectionHeaderProps) {
   return (
     <HeaderWrapper>
       <Title
@@ -79,7 +83,61 @@ export function SectionHeader({ title, header, constrainWidth }: SectionHeaderPr
         }
       >
         {header}
+        {hasCircleFilter && <CircleFilter />}
       </Header>
     </HeaderWrapper>
   );
 }
+
+function CircleFilter() {
+  const [showCircle, setShowCircle] = useState(false);
+  const [{ x, y }, setMousePosition] = useState({ x: 0, y: 0 });
+
+  return (
+    <CircleFilterWrapper
+      onMouseEnter={() => {
+        setShowCircle(true);
+      }}
+      onMouseLeave={() => {
+        setShowCircle(false);
+      }}
+      onMouseMove={(e) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        setMousePosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+      }}
+    >
+      <Circle
+        style={
+          {
+            opacity: showCircle ? 1 : 0,
+            "--x-pos": x.toFixed() + "px",
+            "--y-pos": y.toFixed() + "px",
+          } as CSSProperties
+        }
+      />
+    </CircleFilterWrapper>
+  );
+}
+
+const CircleFilterWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
+
+const Circle = styled.div`
+  position: absolute;
+  --height: 200px;
+  --half-height: calc(var(--height) / 2);
+  height: var(--height);
+  top: calc(var(--y-pos) - var(--half-height));
+  left: calc(var(--x-pos) - var(--half-height));
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  backdrop-filter: brightness(2) saturate(20) hue-rotate(76.5deg);
+  transition: opacity var(--animation-duration);
+  pointer-events: none;
+`;
