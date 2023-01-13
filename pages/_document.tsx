8ac2f55c68cfcb3220cbd0a36797cec0a694a1ca
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-13 09:37:29 +0200

    do not preload fonts
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,7 +29,6 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="description" content={description} />
-
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@UMAprotocol" />
           <meta name="twitter:title" content={title} />
@@ -43,50 +42,15 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
           <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
           <link rel="mask-icon" href="safari-pinned-tab.svg" color="#FF4A4A" />
-          <link
-            rel="preload"
-            href="/fonts/HalyardDisplayLight.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/fonts/HalyardDisplay-Regular.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/fonts/HalyardDisplaySemiBold.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/fonts/HalyardDisplay-Bold.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link rel="preload" href="/fonts/HalyardDisplayLight.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-          <link
-            rel="preload"
-            href="/fonts/HalyardDisplay-Regular.ttf"
-            as="font"
-            type="font/ttf"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/fonts/HalyardDisplaySemiBold.ttf"
-            as="font"
-            type="font/ttf"
-            crossOrigin="anonymous"
-          />
-          <link rel="preload" href="/fonts/HalyardDisplay-Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+          <link href="/fonts/HalyardDisplayLight.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          <link href="/fonts/HalyardDisplay-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          <link href="/fonts/HalyardDisplaySemiBold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          <link href="/fonts/HalyardDisplay-Bold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          <link href="/fonts/HalyardDisplayLight.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+          <link href="/fonts/HalyardDisplay-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+          <link href="/fonts/HalyardDisplaySemiBold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+          <link href="/fonts/HalyardDisplay-Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+          <link rel="preload" as="image" href="/assets/hero-bg-lines.svg" type="image/svg" />
         </Head>
         <body>
           <Main />
