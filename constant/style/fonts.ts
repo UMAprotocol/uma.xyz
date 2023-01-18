613540cd706e57e03029314b7af436c5f5e37c13
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-18 13:36:22 +0200

    rename for clarity
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- constant/style/fonts.ts
+++ constant/style/fonts.ts
@@ -1,17 +1,16 @@
-const regular = 400;
+const weight = 400;
 
 const family = "'Halyard Display', sans-serif";
 
-// New fonts
-export const subHeader = `${regular} 16px/19px ${family}`;
-export const subHeaderSm = `${regular} 12px/16px ${family}`;
-export const headerLg = `${regular} 96px/112px ${family}`;
-export const headerMd = `${regular} 64px/72px ${family}`;
-export const headerSm = `${regular} 40px/55px ${family}`;
-export const headerXs = `${regular} 32px/45px ${family}`;
+export const subHeader = `${weight} 16px/19px ${family}`;
+export const subHeaderSm = `${weight} 12px/16px ${family}`;
+export const headerLg = `${weight} 96px/112px ${family}`;
+export const headerMd = `${weight} 64px/72px ${family}`;
+export const headerSm = `${weight} 40px/55px ${family}`;
+export const headerXs = `${weight} 32px/45px ${family}`;
 
-export const bodyXl = `${regular} 26px/32px ${family}`;
-export const bodyLg = `${regular} 20px/28px ${family}`;
-export const bodyMd = `${regular} 18px/24px ${family}`;
-export const bodySm = `${regular} 16px/22px ${family}`;
-export const bodyXs = `${regular} 12px/17px ${family}`;
+export const bodyXl = `${weight} 26px/32px ${family}`;
+export const bodyLg = `${weight} 20px/28px ${family}`;
+export const bodyMd = `${weight} 18px/24px ${family}`;
+export const bodySm = `${weight} 16px/22px ${family}`;
+export const bodyXs = `${weight} 12px/17px ${family}`;
