b63830fa27bcc7b39f510287dc924033091f18bc
ryanwolhuter <dev@ryanwolhuter.com>
2023-06-27 14:15:22 +0200

    adjust header animation
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
     <motion.header
       initial={{ opacity: 0, translateY: "-100%" }}
       animate={{ opacity: 1, translateY: "0%" }}
-      transition={{ duration: 0.3, delay: 0.8 }}
+      transition={{ duration: 0.2, delay: 0.7 }}
       style={
         {
           "--background": isLightTheme ? white : grey200,
