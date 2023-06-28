5ae7ea6b674e5f1f42fcf457ad2adb20e755bc87
ryanwolhuter <dev@ryanwolhuter.com>
2023-06-28 16:56:19 +0200

    use negative margin for osnap hero bg color
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ import VoteTicker from "./VoteTicker";
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="">
+    <div className="bg-[#2a252a]">
       <VoteTicker />
       <Header />
       <main className="overflow-clip">{children}</main>
