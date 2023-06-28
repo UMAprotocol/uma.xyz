301f8f6673b748825559d5cf852d95bd869510b4
ryanwolhuter <dev@ryanwolhuter.com>
2023-06-28 11:28:25 +0200

    add component
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ src/stories/Osnap/Scaling.stories.tsx
@@ -0,0 +1,16 @@
+import { Scaling } from "@/components/Osnap/Scaling";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta = {
+  component: Scaling,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Scaling>;
+
+const Template: Story = {};
+
+export const Default: Story = {
+  ...Template,
+};
