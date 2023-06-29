6b132b9bbb4c68a945d548485f82a00188391f6a
ryanwolhuter <dev@ryanwolhuter.com>
2023-06-29 11:27:51 +0200

    add white bg
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/Osnap/Hero.tsx
+++ src/components/Osnap/Hero.tsx
@@ -5,7 +5,7 @@ import placeholderImage from "public/assets/placeholder-image.jpg";
 
 export function Hero() {
   return (
-    <section className="mx-auto -mt-[--header-height] min-h-[calc(100dvh_+_var(--header-height))] max-w-[480px] px-6 pt-[--header-blur-height] md:max-w-[1216px]">
+    <section className="mx-auto -mt-[--header-height] min-h-[calc(100dvh_+_var(--header-height))] max-w-[480px] bg-white px-6 pt-[--header-blur-height] md:max-w-[1216px]">
       <div className="relative isolate mb-8 md:grid md:grid-cols-[1.4fr,1fr] md:gap-4">
         <div className="absolute -right-[30px] -top-[7px] -z-10 w-[109px] -rotate-[27deg] rounded-2xl md:hidden">
           <Image src={placeholderImage} alt="placeholder" className=" rounded-xl" />
