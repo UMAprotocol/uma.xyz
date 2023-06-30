b18a187a35771ecb6f6c53f37f5208fa8b8035e8
ryanwolhuter <dev@ryanwolhuter.com>
2023-06-30 11:47:40 +0200

    update images
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/Osnap/Hero.tsx
+++ src/components/Osnap/Hero.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
+import handshake from "public/assets/handshake.png";
 import OsnapLogo from "public/assets/osnap-logo.svg";
-import placeholderImage from "public/assets/placeholder-image.png";
 
 export function Hero() {
   return (
     <section className="mx-auto -mt-[--header-height] max-w-[480px] bg-white px-6 pt-[200px] md:max-w-[1216px]">
       <div className="relative isolate mb-8 md:grid md:grid-cols-[1.4fr,1fr] md:gap-4">
         <div className="absolute -right-[30px] -top-[7px] -z-10 w-[109px] -rotate-[27deg] rounded-2xl md:hidden">
-          <Image src={placeholderImage} alt="placeholder" className=" rounded-xl" />
+          <Image src={handshake} alt="handshake" className=" rounded-xl" />
         </div>
         <div className=" absolute -right-[44px] top-[200px] -z-10 w-[65px] rotate-[15deg] rounded-2xl md:hidden">
-          <Image src={placeholderImage} alt="placeholder" className="rounded-xl" />
+          <Image src={handshake} alt="handshake" className="rounded-xl" />
         </div>
         <div>
           <h1 className="mb-4 flex items-center gap-2 text-xl text-grey-600 md:text-2xl">
@@ -36,7 +36,7 @@ export function Hero() {
           </div>
         </div>
         <div className="hidden aspect-square max-w-[498px] rounded-[56px] md:block">
-          <Image src={placeholderImage} alt="placeholder" className="rounded-[56px]" />
+          <Image src={handshake} alt="handshake" className="rounded-[56px]" />
         </div>
       </div>
     </section>
