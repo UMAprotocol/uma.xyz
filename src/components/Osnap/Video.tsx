d20d259a4e74cec9344ad06293efe538b35cdc06
ryanwolhuter <dev@ryanwolhuter.com>
2023-06-30 11:13:35 +0200

    add youtube embed
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/Osnap/Video.tsx
+++ src/components/Osnap/Video.tsx
@@ -1,3 +1,22 @@
 export function Video() {
-  return <section></section>;
+  return (
+    <section className="bg-white p-5">
+      <div className="relative aspect-video before:absolute before:-inset-1 before:-z-10 before:bg-black before:opacity-75 before:blur-md">
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/R97GIW5M_r0?controls=0"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            clipPath: "inset(0% 0% 0% 0% round 16px)",
+          }}
+        ></iframe>
+      </div>
+    </section>
+  );
 }
