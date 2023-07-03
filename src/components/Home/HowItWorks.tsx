97308fd99a5e96f21f27c24c5b80fdb8fc070476
ryanwolhuter <dev@ryanwolhuter.com>
2023-07-03 14:42:56 +0200

    remove load when in view
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/Home/HowItWorks.tsx
+++ src/components/Home/HowItWorks.tsx
@@ -1,20 +1,16 @@
-import { grey500, grey700, laptopAndUnder, mobileAndUnder, red, tabletAndUnder, white } from "@/constant";
+import { grey500, grey700, red, white } from "@/constant";
 import { useScrollContext } from "@/hooks/contexts/useScrollContext";
 import { useLoadSectionRefAndId } from "@/hooks/helpers/useLoadSectionRefAndId";
-import { motion, useInView, useScroll, useSpring } from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
 import { useEffect, useRef } from "react";
-import styled from "styled-components";
 import { useWindowSize } from "usehooks-ts";
 import { SectionHeader } from "../SectionHeader";
 
 export default function HowItWorks() {
   const { setColorChangeSectionRef } = useScrollContext();
   const ref = useRef<HTMLDivElement>(null);
-  const inView = useInView(ref, { amount: "some" });
   const id = "how-it-works";
-
   useLoadSectionRefAndId(ref, id);
-
   useEffect(() => {
     setColorChangeSectionRef(ref);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -73,7 +69,6 @@ export default function HowItWorks() {
             text={text}
             subText={subText}
             index={index}
-            inView={inView}
             isLast={index === steps.length - 1}
           />
         ))}
@@ -87,10 +82,9 @@ interface StepProps {
   text: string;
   subText: string;
   index: number;
-  inView: boolean;
   isLast: boolean;
 }
-function Step({ header, text, subText, index, inView, isLast }: StepProps) {
+function Step({ header, text, subText, index, isLast }: StepProps) {
   const lineRef = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: lineRef,
@@ -142,181 +136,12 @@ function Step({ header, text, subText, index, inView, isLast }: StepProps) {
         <p className="mb-4 mt-2 text-3xl sm:my-6 sm:text-6xl">{text}</p>
         <p className="sm: text-xl">{subText}</p>
       </div>
-      <div className="border border-grey-400 md:max-w-[754px] lg:col-start-3 lg:row-start-1 lg:max-w-[562px]">
+      <div className="min-h-[400px] border border-grey-400 md:max-w-[754px] lg:col-start-3 lg:row-start-1 lg:max-w-[562px]">
         <video className="h-full w-full object-cover" autoPlay loop muted playsInline>
-          {inView && (
-            <>
-              <source src={`/assets/step-${stepNumber}.mp4`} type="video/mp4" />
-              <source src={`/assets/step-${stepNumber}.webm`} type="video/webm" />
-            </>
-          )}
+          <source src={`/assets/step-${stepNumber}.mp4`} type="video/mp4" />
+          <source src={`/assets/step-${stepNumber}.webm`} type="video/webm" />
         </video>
       </div>
     </div>
   );
 }
-
-const OuterWrapper = styled.section`
-  padding-top: var(--header-blur-height);
-  padding-inline: var(--page-padding);
-  background: linear-gradient(180deg, var(--white) 0%, var(--white-200) 50%, var(--white) 100%);
-`;
-
-const InnerWrapper = styled.div`
-  max-width: var(--page-width);
-  margin-inline: auto;
-`;
-
-const StepWrapper = styled.div`
-  display: grid;
-  grid-template-columns: auto 1fr auto;
-  grid-template-rows: auto;
-  grid-template-areas: "number description lottie";
-  padding-bottom: var(--step-padding-bottom);
-  --step-padding-bottom-desktop: 96px;
-  --step-padding-bottom-mobile: 22px;
-  --step-padding-bottom: var(--step-padding-bottom-desktop);
-  @media ${laptopAndUnder} {
-    grid-template-columns: auto auto;
-    grid-template-rows: auto auto;
-    grid-template-areas:
-      "description description"
-      "number lottie";
-    grid-column-gap: 52px;
-  }
-  @media ${mobileAndUnder} {
-    --step-padding-bottom: var(--step-padding-bottom-mobile);
-    grid-template-columns: auto;
-    grid-template-rows: auto auto;
-    grid-template-areas:
-      "description"
-      "lottie";
-    grid-column-gap: unset;
-  }
-  &:is(:last-child) {
-    padding-bottom: 0;
-  }
-`;
-
-const Video = styled.video`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
-
-const LottieWrapper = styled.div`
-  max-width: var(--width);
-  border: 1px solid var(--grey-400);
-  grid-area: lottie;
-  --width-desktop: 562px;
-  --width-laptop-tablet: 754px;
-  --width-mobile: 100%;
-  --width: var(--width-desktop);
-  @media ${laptopAndUnder} {
-    --width: var(--width-laptop-tablet);
-  }
-  @media ${mobileAndUnder} {
-    --width: var(--width-mobile);
-  }
-`;
-
-const StepDescription = styled.div`
-  grid-area: description;
-  max-width: var(--width);
-  margin-top: 16px;
-  --width-laptop-desktop: 464px;
-  --width-tablet: 720px;
-  --width-mobile: 100%;
-  --width: var(--width-laptop-desktop);
-  @media ${laptopAndUnder} {
-    margin-top: 0;
-    margin-bottom: 96px;
-  }
-  @media ${tabletAndUnder} {
-    --width: var(--width-tablet);
-  }
-  @media ${mobileAndUnder} {
-    --width: var(--width-mobile);
-    margin-bottom: 42px;
-  }
-`;
-
-const StepHeader = styled.h3`
-  text-transform: capitalize;
-  font: var(--sub-header);
-  color: var(--red);
-
-  @media ${mobileAndUnder} {
-    font: var(--sub-header-sm);
-  }
-`;
-
-const StepText = styled.p`
-  margin-block: 24px;
-  font: var(--header-md);
-  @media ${mobileAndUnder} {
-    font: var(--header-xs);
-    margin-top: 8px;
-    margin-bottom: 16px;
-  }
-`;
-
-const StepSubText = styled.p`
-  font: var(--body-lg);
-
-  @media ${mobileAndUnder} {
-    font: var(--body-sm);
-  }
-`;
-
-const StepNumberWrapper = styled.div`
-  grid-area: number;
-  --step-number-offset: -56px;
-  --step-number-size: 48px;
-  margin-left: calc(var(--step-number-offset) - var(--step-number-size));
-
-  @media ${laptopAndUnder} {
-    margin-left: 0;
-    justify-self: end;
-  }
-
-  @media ${mobileAndUnder} {
-    display: none;
-  }
-`;
-
-const StepNumber = styled(motion.p)`
-  display: grid;
-  place-items: center;
-  width: var(--step-number-size);
-  height: var(--step-number-size);
-  border-radius: 8px;
-  font: var(--body-sm);
-  margin: 0;
-  padding: 0;
-  transition: background-color var(--animation-duration), color var(--animation-duration),
-    border-color var(--animation-duration);
-`;
-
-const StepLineOuter = styled.div`
-  z-index: -1;
-  background: var(--grey-500);
-  height: calc(100% + var(--step-padding-bottom));
-  width: 1px;
-  margin-left: calc(var(--step-number-size) / 2);
-
-  @media ${laptopAndUnder} {
-    height: 100%;
-    background: linear-gradient(180deg, var(--grey-500) 0%, rgba(176, 175, 179, 0) 100%);
-  }
-`;
-
-const StepLineInner = styled(motion.div)`
-  height: 100%;
-  width: 1px;
-  background: var(--red);
-  transform-origin: top;
-  @media ${laptopAndUnder} {
-    background: linear-gradient(180deg, var(--red) 0%, rgba(176, 175, 179, 0) 100%);
-  }
-`;
