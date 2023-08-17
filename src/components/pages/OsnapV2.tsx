235beef44502211219bb187520db999cb0b762e0
ryanwolhuter <dev@ryanwolhuter.com>
2023-08-17 13:53:48 +0200

    fix alignment
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/pages/OsnapV2.tsx
+++ src/components/pages/OsnapV2.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "../OsnapV2/Hero";
+import { Steps } from "../OsnapV2/Steps";
 
 export function OsnapV2() {
   return (
     <div className="bg-white">
       <Hero />
+      <Steps />
     </div>
   );
 }
