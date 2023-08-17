e6d171dd6895a4d9498012f191f7f8f052e30c70
Ryan Wolhuter <dev@ryanwolhuter.com>
2023-08-17 16:37:35 +0200

    feat: features (#97)
    * add mobile styles
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>
    
    * add large styles
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>
    
    * fix alignment
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>
    
    * add description section
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>
    
    * add description to page
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>
    
    * add features section
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>
    
    ---------
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ src/stories/OsnapV2/Features.stories.tsx
@@ -0,0 +1,16 @@
+import { Features } from "@/components/OsnapV2/Features";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta = {
+  component: Features,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Features>;
+
+const Template: Story = {};
+
+export const Default: Story = {
+  ...Template,
+};
