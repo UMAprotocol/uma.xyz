bde944680b8143b0cd269c7f243f9aea4b5e89b6
Gerhard Steenkamp <gerhard.m.steenkamp@gmail.com>
2023-10-27 22:11:07 +0200

    let env var format the value

--- src/components/OsnapV2/TvsChip.tsx
+++ src/components/OsnapV2/TvsChip.tsx
@@ -1,16 +1,14 @@
 import { GradientBorder, GradientBorderProps } from "@/components/GradientBorder";
 
 type TvsChipProps = GradientBorderProps & {
-  amount: number; // eg. 83_000_000
+  amount: string; // eg. $ 83M
 };
 
 export function TvsChip({ amount, className, ...props }: TvsChipProps) {
-  const formattedAmount = (amount / 10 ** 6).toLocaleString();
-
   return (
     <GradientBorder hoverEffect className={className} {...props}>
       <div className="flex items-baseline gap-[0.20em] px-2 py-1 text-lg lg:text-xl">
-        <span className="font-bold text-primary-500">+${formattedAmount}M</span>
+        <span className="font-bold text-primary-500">${amount}+</span>
         Total Value Secured
       </div>
     </GradientBorder>
