af133bc7fcff18f4f1c3057e1a4438c633b2ab68
Gerhard Steenkamp <gerhard.m.steenkamp@gmail.com>
2023-10-27 17:57:01 +0200

    show hover effect

--- src/components/OsnapV2/TvsChip.tsx
+++ src/components/OsnapV2/TvsChip.tsx
@@ -8,7 +8,7 @@ export function TvsChip({ amount, className, ...props }: TvsChipProps) {
   const formattedAmount = (amount / 10 ** 6).toLocaleString();
 
   return (
-    <GradientBorder className={className} {...props}>
+    <GradientBorder hoverEffect className={className} {...props}>
       <div className="flex items-baseline gap-[0.20em] px-2 py-1 text-lg lg:text-xl">
         <span className="font-bold text-primary-500">+${formattedAmount}M</span>
         Total Value Secured
