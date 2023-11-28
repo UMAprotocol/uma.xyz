1dabcb96aea54b72e240b8c2e9df8bc2ba04bff9
Gerhard Steenkamp <gerhard.m.steenkamp@gmail.com>
2023-11-28 14:42:54 +0200

    update icons, use links

--- src/components/OsnapV2/Partners.tsx
+++ src/components/OsnapV2/Partners.tsx
@@ -1,25 +1,31 @@
 import { Icon } from "../Icon";
+import NextLink from "next/link";
 
 const partners = [
   {
     label: "Connext",
     icon: "connext",
+    href: "https://www.connext.network/",
   },
   {
     label: "Cow Swap",
     icon: "cow-swap",
+    href: "https://cow.fi/",
   },
   {
     label: "Premia",
     icon: "premia",
+    href: "https://premia.blue/",
   },
   {
     label: "Shape Shift",
     icon: "shape-shift",
+    href: "https://shapeshift.com/",
   },
   {
     label: "Across",
     icon: "across-with-text",
+    href: "https://across.to/",
   },
 ];
 
@@ -29,12 +35,13 @@ export const Partners = () => {
       <h2 className="text-gray-600 text-md xl:text-lg">Securing Crypto&apos;s Finest</h2>
       <div className="flex w-full flex-row flex-wrap gap-6">
         {partners.map((partner) => (
-          <Icon
-            className="bg-gray-600 max-h-10 min-w-[160px] flex-1 text-grey-900"
-            aria-label={`Corporate logo for ${partner.label}.`}
-            key={partner.label}
-            name={partner.icon}
-          />
+          <NextLink key={partner.label} href={partner.href} target="_blank" className="max-h-10 min-w-[160px] flex-1">
+            <Icon
+              className="h-full w-full text-black"
+              aria-label={`Corporate logo for ${partner.label}.`}
+              name={partner.icon}
+            />
+          </NextLink>
         ))}
       </div>
     </section>
