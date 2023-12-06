4b13fcfcfc92f43ab687628dc115688f5efd8d8b
Gerhard Steenkamp <gerhard@umaproject.org>
2023-12-06 14:11:23 +0200

    fix spacing on larger screens

--- src/components/Oval/OevCreation.tsx
+++ src/components/Oval/OevCreation.tsx
@@ -34,7 +34,7 @@ const content = [
 
 export const OevCreation = () => {
   return (
-    <section className="relative mx-auto mt-12 flex max-w-[700px] flex-col items-center gap-8 px-[--page-padding] text-center align-top xl:max-w-[1300px]">
+    <section className="relative mx-auto mt-12 flex max-w-[700px] flex-col items-center gap-8 px-[--page-padding] text-center align-top xl:max-w-[1300px] xl:gap-[96px]">
       <h2 className="text-gradient-oval px-[20%] text-center text-sm-fluid md:text-md-fluid lg:text-lg-fluid">
         OEV Creation
       </h2>
@@ -44,7 +44,7 @@ export const OevCreation = () => {
         ))}
       </div>
       <Link
-        className="w-full justify-self-end whitespace-nowrap rounded-lg bg-red px-6 py-4 text-lg text-background no-underline transition hover:opacity-75 xl:mt-8 xl:w-fit"
+        className="w-full justify-self-end whitespace-nowrap rounded-lg bg-red px-6 py-4 text-lg text-background no-underline transition hover:opacity-75 xl:w-fit"
         href="https://demo.oval.uma.xyz/"
         target="_blank"
       >
