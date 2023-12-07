898b1467221fbaeb037f03f689881491d1b60c65
Gerhard Steenkamp <gerhard@umaproject.org>
2023-12-07 15:55:47 +0200

    use tailwind merge for better default styles

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,10 +1,12 @@
+import { cn } from "@/utils/styleUtils";
+
 type Props = React.ComponentPropsWithoutRef<"svg"> & {
   name: string;
   className?: string;
 };
 export function Icon({ name, className, ...props }: Props) {
   return (
-    <svg className={`h-[1em] w-[1em] ${className}`} {...props}>
+    <svg className={cn("h-[1em] w-[1em]", className)} {...props}>
       <use href={`assets/icons.svg#${name}`} />
     </svg>
   );
