37b4091eded686a198e56d65677b435478e7ccdf
Gerhard Steenkamp <gerhard@umaproject.org>
2024-01-09 13:21:25 +0200

    capture oev

--- src/components/Oval/CaptureOev.tsx
+++ src/components/Oval/CaptureOev.tsx
@@ -2,18 +2,18 @@ import { cn } from "@/utils/styleUtils";
 import { Divider } from "./Divider";
 
 const content = [
-  "Searcher bots extract Maximal Extractable Value by interfering with Ethereum block production.",
-  "Price oracles leave protocols vulnerable to a subset of MEV - called Oracle Extractable Value.",
-  "Oval wraps your existing price feed, shields you from searchers, and auctions your OEV.",
+  "Oracle price updates create a type of MEV called Oracle Extractable Value.",
+  "Searcher bots and block builders profit by working together to extract OEV from protocols.",
+  "Oval wraps your Chainlink Data Feed and forces searchers to pay you for the right to extract OEV.",
 ];
 
 export const CaptureOev = () => {
   return (
     <section className="relative mx-auto mt-12 flex max-w-[1200px] flex-col items-center gap-4 px-[--page-padding] text-center align-top">
-      <h2 className="text-gradient-oval px-[20%] text-center text-sm-fluid md:text-md-fluid lg:text-lg-fluid">
-        Capture OEV with Oval
+      <h2 className="text-gradient-oval px-[10%] text-center text-sm-fluid md:text-md-fluid lg:text-lg-fluid">
+        Reclaim Oracle Extractable Value with Oval
       </h2>
-      <h3 className="bg-gradient-to-r from-white to-[hsla(0,0%,72%,1)] bg-clip-text text-center text-xl text-transparent">
+      <h3 className="text:md bg-gradient-to-r from-white to-[hsla(0,0%,72%,1)] bg-clip-text text-center text-transparent opacity-75 lg:text-sm-fluid">
         Oracle Value Aggregation Layer
       </h3>
       <div className="mt-8 flex flex-col gap-2 lg:gap-3 xl:flex-row xl:items-center xl:gap-0">
@@ -38,11 +38,11 @@ const Card = ({ number, text, className }: CardProps) => {
   return (
     <div
       className={cn(
-        "flex flex-1 flex-row items-start gap-4 rounded-[20px] border border-white/5 bg-white/5 p-6 text-lg text-white shadow-sm backdrop-blur-sm",
+        "flex flex-1 flex-row items-center gap-4 rounded-[20px] border border-white/5 bg-white/5 p-6 text-lg text-white shadow-sm backdrop-blur-sm",
         className,
       )}
     >
-      <div className="text-md aspect-square h-[2em] w-[2em] rounded-[10px] border border-white/5 text-center text-white/50 shadow-md">
+      <div className="text-md mb-auto aspect-square h-[2em] w-[2em] rounded-[10px] border border-white/5 text-center text-white/50 shadow-md">
         {number}
       </div>
       <span className="text-left opacity-75">{text}</span>
