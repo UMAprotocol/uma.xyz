075331ebee9097138287cac616f8bda785a6868f
Gerhard Steenkamp <gerhard@umaproject.org>
2024-01-12 18:28:09 +0200

    Revert "use full URL from server component"
    This reverts commit 5f67a94a8c83ed53ac1f8231006b6826efefdd10.

--- src/queries/getOevLost.ts
+++ src/queries/getOevLost.ts
@@ -1,12 +1,10 @@
 import { OevDataResponse } from "@/app/api/oev-data/route";
-import { getApiRouteUrl } from "@/utils";
 
 // should this be an env variable?
 const ONE_DAY_SECONDS = 86_400;
 
 export async function getOevLost() {
-  const URI = getApiRouteUrl("/api/oev-data");
-  const response = await fetch(URI, {
+  const response = await fetch("/api/oev-data", {
     next: {
       revalidate: ONE_DAY_SECONDS,
     },
