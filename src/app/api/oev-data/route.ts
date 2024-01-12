94ac1c32508fdf869a283e72f56dec75b8143693
Gerhard Steenkamp <gerhard@umaproject.org>
2024-01-12 17:50:18 +0200

    use dune client to better handle long running queries

--- src/app/api/oev-data/route.ts
+++ src/app/api/oev-data/route.ts
@@ -1,57 +1,22 @@
 import { NextResponse } from "next/server";
+import { DuneClient } from "@cowprotocol/ts-dune-client";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const API_KEY = process.env.DUNE_API_KEY!;
+const Dune = new DuneClient(API_KEY);
 
-// should this be an env variable?
-const ONE_DAY_SECONDS = 86_400;
-const QUERY_ID = "1258228";
-
-type ExecutionResponse =
-  | {
-      execution_id: string;
-      state: "QUERY_STATE_PENDING";
-    }
-  | {
-      execution_id: undefined;
-      state: "QUERY_STATE_FAILED"; // not sure
-    };
-
-type DuneResponse = {
-  oevLost: number;
-};
-
-const headers = new Headers({
-  "x-dune-api-key": API_KEY,
-});
+const QUERY_ID = 1258228;
 
 export async function GET() {
   try {
-    const executionRes = await fetch(`https://api.dune.com/api/v1/query/${QUERY_ID}`, {
-      method: "POST",
-      headers,
-      next: {
-        revalidate: ONE_DAY_SECONDS,
-      },
-    });
-    // what if the execution request fails? how do we return stale resultsData? Redis?
-    if (!executionRes.ok) {
-      throw new Error("Network error fetching Oev data");
-    }
-    const executionData = (await executionRes.json()) as ExecutionResponse;
-    if (executionData.state === "QUERY_STATE_FAILED") {
+    const executionRes = await Dune.refresh(QUERY_ID);
+
+    if (!executionRes.result) {
       throw new Error("Failed to execute query");
     }
-    const executionId = executionData.execution_id;
-    const resultsRes = await fetch(`https://api.dune.com/api/v1/execution/${executionId}/results`, {
-      method: "GET",
-      headers,
-    });
-
-    const resultsData = (await resultsRes.json()) as DuneResponse;
     return NextResponse.json({
       status: 200,
-      body: { oevLost: resultsData.oevLost },
+      body: { oevLost: executionRes.result.rows[0] }, // find where value lives
     });
   } catch (e) {
     console.error(e);
--- src/queries/getOevLost.ts
+++ src/queries/getOevLost.ts
@@ -1,7 +1,14 @@
 import { OevDataResponse } from "@/app/api/oev-data/route";
 
+// should this be an env variable?
+const ONE_DAY_SECONDS = 86_400;
+
 export async function getOevLost() {
-  const response = await fetch("/api/oev-data");
+  const response = await fetch("/api/oev-data", {
+    next: {
+      revalidate: ONE_DAY_SECONDS,
+    },
+  });
   const res = (await response.json()) as OevDataResponse;
   return res.oevLost;
 }
