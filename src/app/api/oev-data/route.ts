a245824b5e109c1831400299a670a6cf863eaee4
Gerhard Steenkamp <gerhard@umaproject.org>
2024-01-24 19:04:34 +0200

    make countup a client component

--- src/app/api/oev-data/route.ts
+++ src/app/api/oev-data/route.ts
@@ -10,17 +10,17 @@ const QUERY_ID = 3373179;
 export async function GET() {
   try {
     const executionRes = await Dune.refresh(QUERY_ID);
-    console.log(executionRes)
+    console.log(executionRes);
 
     if (!executionRes.result) {
       throw new Error("Failed to execute query");
     }
 
-    const oevLost = (executionRes.result.rows[0] as {'max_potential_revenue_usd': number}).max_potential_revenue_usd
+    const oevLost = (executionRes.result.rows[0] as { max_potential_revenue_usd: number }).max_potential_revenue_usd;
     // cache successful response
     return NextResponse.json({
       status: 200,
-      body: { oevLost }, // find where value lives
+      oevLost, // find where value lives
     });
   } catch (e) {
     console.error(e);
@@ -31,7 +31,3 @@ export async function GET() {
     });
   }
 }
-
-export type OevDataResponse = {
-  oevLost: number;
-};
--- src/components/Oval/OevLost.tsx
+++ src/components/Oval/OevLost.tsx
@@ -1,70 +1,34 @@
-"use client";
-import { useEffect, useRef, useState } from "react";
-import { useCountUp } from "use-count-up";
-import { oevLost } from "@/constant/env";
-import { Roboto_Mono } from "next/font/google";
-import { useIntersectionObserver } from "usehooks-ts";
 import { Divider } from "./Divider";
 import { Ellipse } from "./Ellipsis";
+import { headers } from "next/headers";
+import { getApiRouteUrl } from "@/utils";
+import { OevDataResponse } from "@/app/api/oev-data/utils";
+import { Countup } from "./Countup";
 
-// If loading a variable font, you don't need to specify the font weight
-const roboto = Roboto_Mono({
-  weight: "400",
-  subsets: ["latin"],
-});
+const ONE_DAY_SECONDS = 86_400;
 
-export const OevLost = () => {
-  const [startCount, setStartCount] = useState(false);
-  const ref = useRef<HTMLDivElement>(null);
-  const entry = useIntersectionObserver(ref, {
-    threshold: 0,
+export async function getOevLost() {
+  const host = headers().get("host");
+  const URI = getApiRouteUrl("/api/oev-data", host);
+  console.log(URI);
+  const response = await fetch(URI, {
+    next: {
+      revalidate: ONE_DAY_SECONDS,
+    },
   });
+  console.log(response);
+  const res = (await response.json()) as OevDataResponse;
+  return res.oevLost;
+}
 
-  const isVisible = !!entry?.isIntersecting;
-
-  const { value } = useCountUp({
-    isCounting: startCount,
-    start: 0,
-    end: parseInt(oevLost),
-    duration: 3,
-    easing: "easeOutCubic",
-    formatter: (num: number) => Math.ceil(num).toLocaleString(),
-    decimalPlaces: 0,
-  });
-
-  useEffect(() => {
-    if (isVisible) {
-      setStartCount(true);
-    }
-  }, [isVisible]);
+export const OevLost = async () => {
+  const oevLost = await getOevLost();
+  console.log(oevLost);
 
   return (
     <section className="relative mx-auto mb-[150px] flex max-w-[828px] flex-col items-center gap-2 px-[--page-padding] text-center xl:mb-[200px]">
       <Ellipse className="right-[-20%]" />
-      <div
-        style={{
-          perspective: "500px",
-        }}
-      >
-        <h3 className="text-md uppercase leading-6 tracking-widest text-white opacity-50">
-          TOTAL OEV LEAKED BY AAVE V2 & V3, COMPOUND V2 & V3
-        </h3>
-        <div
-          ref={ref}
-          className={`bg-gradient-to-r from-white to-[hsla(0,0%,72%,1)] bg-clip-text text-sm-fluid text-transparent sm:text-md-fluid lg:text-lg-fluid ${roboto.className}`}
-        >
-          ${value}
-        </div>
-        <div
-          style={{
-            transform: "rotate3d(1, 0, 0, 40deg) translateY(1.1em) rotateX(180deg)",
-            transformOrigin: "center top",
-          }}
-          className={`select-none text-sm-fluid text-white opacity-5 sm:text-md-fluid lg:text-lg-fluid  ${roboto.className}`}
-        >
-          ${value}
-        </div>
-      </div>
+      <Countup number={oevLost} />
       <Divider className="mt-[100px] w-[120px] rotate-90 lg:mt-[150px] lg:w-[200px]" />
     </section>
   );
