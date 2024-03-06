923c79e5d8c2c06556364b116fb74a449bbd2949
Gerhard Steenkamp <gerhard@umaproject.org>
2024-03-06 14:26:16 +0200

    add oracle tvs dune query

--- src/lib/dune.ts
+++ src/lib/dune.ts
@@ -3,10 +3,13 @@ import {
   Dune,
   OEV_LOST_KEY,
   OEV_LOST_QUERY_ID,
+  ORACLE_TVS_KEY,
+  ORACLE_TVS_QUERY_ID,
+  OSNAP_TVS_KEY,
   OSNAP_TVS_QUERY_ID,
   OevLostData,
+  OracleTvsData,
   OsnapTvsData,
-  TVS_KEY,
 } from "./constants";
 import { kv } from "@vercel/kv";
 
@@ -30,11 +33,16 @@ export const dune = async <TData>(queryId: number, queryKey: string): Promise<TD
 };
 
 export const getOsnapTvs = cache(async () => {
-  const newData = await dune<OsnapTvsData>(OSNAP_TVS_QUERY_ID, TVS_KEY);
+  const newData = await dune<OsnapTvsData>(OSNAP_TVS_QUERY_ID, OSNAP_TVS_KEY);
   return newData.amount_usd;
 });
 
 export const getOevLost = cache(async () => {
   const newData = await dune<OevLostData>(OEV_LOST_QUERY_ID, OEV_LOST_KEY);
   return newData.max_potential_revenue_usd;
+});
+
+export const getOracleTvs = cache(async () => {
+  const newData = await dune<OracleTvsData>(ORACLE_TVS_QUERY_ID, ORACLE_TVS_KEY);
+  return newData.tvs;
 });
