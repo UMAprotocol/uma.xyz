28001074bc5a097858b5822afda86ba7460847ca
David A <4429761+daywiss@users.noreply.github.com>
2024-03-25 07:43:40 -0400

    fix: redirect various routes to external pages (#165)
    Signed-off-by: david <david@umaproject.org>

--- next.config.js
+++ next.config.js
@@ -8,6 +8,30 @@ const nextConfig = {
       fullUrl: true,
     },
   },
+  redirects: async () => {
+    return [
+      {
+        source: "/jobs",
+        destination: "https://jobs.lever.co/risklabs",
+        permanent: false,
+      },
+      {
+        source: "/products",
+        destination: "https://projects.uma.xyz",
+        permanent: false,
+      },
+      {
+        source: "/faq",
+        destination: "https://docs.uma.xyz/protocol-overview/dvm-2.0-faq",
+        permanent: false,
+      },
+      {
+        source: "/uma.tokenlist.json",
+        destination: "https://raw.githubusercontent.com/UMAprotocol/website/master/public/uma.tokenlist.json",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
