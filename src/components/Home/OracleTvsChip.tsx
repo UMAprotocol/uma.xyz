656c7a5a1c2301c819c9bdca371a70a28248f3c2
Gerhard Steenkamp <51655063+gsteenkamp89@users.noreply.github.com>
2024-04-03 09:27:57 +0200

    style oracle tvs chip to new design (#169)

--- src/components/Home/OracleTvsChip.tsx
+++ src/components/Home/OracleTvsChip.tsx
@@ -1,22 +1,34 @@
-import { GradientBorder, GradientBorderProps } from "@/components/GradientBorder";
 import { totalValueSecured } from "@/constant";
 import { ONE_DAY_SECONDS, duneActive } from "@/lib/constants";
 import { getOracleTvs } from "@/lib/dune";
 import { roundToNearestMillion } from "@/utils";
+import { cn } from "@/utils/styleUtils";
 
 export const revalidate = ONE_DAY_SECONDS;
 
-type TvsChipProps = GradientBorderProps;
+type TvsChipProps = {
+  className?: string;
+};
 
-export async function OracleTvsChip({ className, ...props }: TvsChipProps) {
+export async function OracleTvsChip({ className }: TvsChipProps) {
   const tvs = duneActive ? roundToNearestMillion(await getOracleTvs()) : parseInt(totalValueSecured);
 
   return (
-    <GradientBorder className={className} {...props}>
-      <div className="flex items-baseline gap-[0.20em] px-2 py-1 text-lg lg:text-xl">
-        <span className="font-bold text-primary-500">${tvs}M</span>
-        Total Value Secured
+    <div
+      className={cn(
+        "relative overflow-hidden rounded-2xl bg-gradient-to-b from-transparent to-white/10 shadow-sm",
+        className,
+      )}
+    >
+      <div className="rounded-2xl border border-white/5 p-1">
+        <div className="flex items-baseline gap-[0.20em] rounded-xl border border-white/5 bg-transparent px-3 py-2 text-xl text-white lg:text-2xl">
+          <span className="font-normal text-primary-500">${tvs}M</span>
+          Total Value Secured
+        </div>
       </div>
-    </GradientBorder>
+
+      <span className="z-2 absolute -right-6 -top-5 h-10 w-10 bg-grey-400 opacity-50 blur-md" />
+      <span className="z-2 absolute -bottom-8 -left-7 h-10 w-10 bg-grey-400 opacity-50 blur-md" />
+    </div>
   );
 }
