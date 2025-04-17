a5936b1695cdfb1c1500ae7df1407dff9a5b86e3
david <david@umaproject.org>
2025-04-17 13:12:36 -0400

    improve: update projects, apr, some wodering
    Signed-off-by: david <david@umaproject.org>

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -1,4 +1,4 @@
-export const defaultApr = process.env.NEXT_PUBLIC_DEFAULT_APR ?? "30.1";
+export const defaultApr = process.env.NEXT_PUBLIC_DEFAULT_APR ?? "21";
 export const overrideApr: string | undefined = process.env.NEXT_PUBLIC_OVERRIDE_APR;
 export const totalValueSecured = process.env.NEXT_PUBLIC_TVS ?? "0";
 export const oevLostFallback = process.env.NEXT_PUBLIC_OEV_LOST ?? "0";
